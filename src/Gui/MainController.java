35cca027ffb7dee72da3b856cc1f2e4a70121b50
OliSparrow <jay_jakobsen@outlook.dk>
2023-11-07 10:45:13 +0100

    Beginning implementation of word search

--- src/Gui/MainController.java
+++ src/Gui/MainController.java
@@ -1,4 +1,21 @@
 package Gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class MainController {
+    @FXML
+    private TextField inputWord;
+
+    //Method for Search button:
+    public void searchWord(){
+        if(inputWord != null) {
+            String wordToSearch = inputWord.getText().trim();
+            //Retrieves the word from input
+            System.out.println("Searched word: " + wordToSearch);
+        } else {
+            System.out.println("TextField is not initialized properly.");
+        }
+    }
+
 }
